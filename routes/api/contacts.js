5df176d7c2eef60695a29939a1002117fc5de144
MazkaAndrii <andriy.mazka@gmail.com>
2023-04-22 17:57:15 +0300

    add middle

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
+const { isValidId } = require("../../middlewares");
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", isValidId, ctrl.getById);
 
 router.post("/", ctrl.add);
 
-router.delete("/:contactId", ctrl.remove);
+router.delete("/:contactId", isValidId, ctrl.remove);
 
-router.put("/:contactId", ctrl.update);
+router.put("/:contactId", isValidId, ctrl.update);
 
-router.patch("/:contactId/favorite", ctrl.updateStatusContact);
+router.patch("/:contactId/favorite", isValidId, ctrl.updateStatusContact);
 
 module.exports = router;

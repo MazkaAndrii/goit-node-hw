3c2a5e92e39c677d973a4a30c941345e753db3cf
MazkaAndrii <andriy.mazka@gmail.com>
2023-04-28 19:33:25 +0300

    fix routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -13,14 +13,15 @@ const router = express.Router();
 
 router.get("/", auth, ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", isValidId, ctrlWrapper(ctrl.getById));
+router.get("/:contactId", auth, isValidId, ctrlWrapper(ctrl.getById));
 
 router.post("/", auth, validation(joiSchema), ctrlWrapper(ctrl.add));
 
-router.delete("/:contactId", isValidId, ctrlWrapper(ctrl.remove));
+router.delete("/:contactId", auth, isValidId, ctrlWrapper(ctrl.remove));
 
 router.put(
   "/:contactId",
+  auth,
   isValidId,
   validation(joiSchema),
   ctrlWrapper(ctrl.update)
@@ -28,6 +29,7 @@ router.put(
 
 router.patch(
   "/:contactId/favorite",
+  auth,
   isValidId,
   validation(joiSchemaFavorite),
   ctrlWrapper(ctrl.updateStatusContact)
